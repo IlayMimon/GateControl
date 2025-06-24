74ff106602bf7a0526ca8d793e02026c6a1f28a0
Noam Cohen <noamcohen@h-Air-sl-Noam.lan>
2025-06-24 18:26:36 +0300

    almost ready before marge with NP

--- src/components/GateControl.tsx
+++ src/components/GateControl.tsx
@@ -1,25 +1,30 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { Input } from "antd";
-import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import NoDataPicture from "../assets/pictures/no-data.png";
-import { usePeopleContext } from "../context/PeopleContext";
-import PersonItem from "./PersonItem";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { Input, Select } from 'antd';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import NoDataPicture from '../assets/pictures/no-data.png';
+import { usePeopleContext } from '../context/PeopleContext';
+import PersonItem from './PersonItem';
 
 const { Search } = Input;
 
 interface IGateControlProps {
-  mode: "action" | "status";
+  mode: 'action' | 'status';
 }
 
 function GateControl({ mode }: IGateControlProps) {
   const { peopleData, peopleIsLoading } = usePeopleContext();
   const [searchValue, setSearchValue] = useState<string | undefined>();
+  const [addingPerson, setAddingPerson] = useState<boolean>(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [armyId, setArmyId] = useState(searchValue);
+  const [branch, setBranch] = useState<string | undefined>(undefined);
   const [searchParams] = useSearchParams();
-  const location = searchParams.get("location");
+  const location = searchParams.get('location');
 
   let filteredData =
-    mode === "status" && location
+    mode === 'status' && location
       ? peopleData?.filter((person) => person.Location === location)
       : peopleData;
 
@@ -44,11 +49,73 @@ function GateControl({ mode }: IGateControlProps) {
             />
           </div>
         ) : filteredData?.length ? (
-          filteredData.map((person) => <PersonItem key={person.ID} person={person} mode={mode} />)
+          filteredData.map((person) => (
+            <PersonItem key={person.ID} person={person} mode={mode} />
+          ))
         ) : (
           <div className="gate-control__no-data">
-            <img src={NoDataPicture} alt="No data" />
-            <span>אין מידע</span>
+            {!addingPerson ? (
+              <>
+                <img src={NoDataPicture} alt="No data" />
+                <span>אין מידע</span>
+                {mode === 'action' && (
+                  <div className="gate-control__no-data-action">
+                    <button
+                      className="gate-control__no-data-button"
+                      onClick={() => setAddingPerson(true)}
+                    >
+                      להוספה לחצ/י
+                    </button>
+                  </div>
+                )}
+              </>
+            ) : (
+              <div className="gate-control__add-person">
+                <Input
+                  placeholder="שם פרטי"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Input
+                  placeholder="שם משפחה"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <Input
+                  placeholder="מספר אישי"
+                  defaultValue={searchValue}
+                  value={armyId}
+                  onChange={(e) => setArmyId(e.target.value)}
+                />
+                <Select
+                  placeholder="בחר אגף"
+                  value={branch}
+                  onChange={(value) => setBranch(value)}
+                >
+                  {/* {branch.map((branch) => (
+                    <Select.Option key={branch} value={branch}>
+                      {branch}
+                    </Select.Option>
+                  ))} */}
+                </Select>
+                <div className="gate-control__add-buttons">
+                  <button
+                    className="gate-control__add-button"
+                    onClick={() => {
+                      setAddingPerson(false);
+                    }}
+                  >
+                    הוספה
+                  </button>
+                  <button
+                    className="gate-control__cancel-button"
+                    onClick={() => setAddingPerson(false)}
+                  >
+                    ביטול
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
