138566a1038bf94cba7f9f4d7251ee1e5dce4bd4
Noam Cohen <noamcohen@h-MacBook-Air-sl-Noam.local>
2026-03-02 16:10:38 +0200

    fixing some changes

--- src/components/KPI.tsx
+++ src/components/KPI.tsx
@@ -8,7 +8,7 @@ function KPI({ title, value, icon }: IKPIProps) {
     <div className="kpi">
       <span className="kpi__icon">{icon}</span>
       <span className="kpi__title">
-        {title} - <span className="kpi__title--value">{value}</span>
+        {title} <span className="kpi__title--value">{value}</span>
       </span>
     </div>
   );
